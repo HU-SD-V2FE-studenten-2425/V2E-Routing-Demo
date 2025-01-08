d3df591de9564a12b65a260fd1ba4f8e94bcd2be
Alex Jongman <alex.jongman@hu.nl>
2025-01-08 10:46:25 +0100

    courses fetched from service

--- src/view/components/courses-list.js
+++ src/view/components/courses-list.js
@@ -1,7 +1,31 @@
 import { LitElement, css, html } from 'lit';
+import { coursesController } from '../../controller/courses-controller';
 
 export class CoursesList extends LitElement {
+
+  static properties = {
+    courses: { type: Array }
+  }
+
+  constructor() {
+    super();
+    this.courses = [];
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    coursesController.getCourses()
+    .then((courses) => {
+      this.courses = courses;
+      console.log('connectedCallback', this.courses);
+    })
+  }
+    
+
   render() {
+
+    console.log('render', this.courses);
+
     return html`
       <h1>Courses</h1>
       <ul>
